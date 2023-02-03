6401b12f5eb58d35fbf76e50c0447c9ae31fbd92
mortinger91 <micheleemiliani91@gmail.com>
2023-02-03 22:03:01 +0100

    testing actions 3

--- ProjectFolder/scripts/run_tests.sh
+++ ProjectFolder/scripts/run_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Running tests..."
-ProjectFolder/scripts/cmake_build.sh
-ProjectFolder/scripts/make_debug.sh
-cd ProjectFolder/build/debug
-make test
+ProjectFolder/scripts/cmake_build.sh && \
+ProjectFolder/scripts/make_debug.sh && \
+make test -C ProjectFolder/build/debug
 echo "Finished running tests..."
