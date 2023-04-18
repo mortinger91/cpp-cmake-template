75c93e9424797a80ba411cb309bf089df45033ee
mortinger91 <micheleemiliani91@gmail.com>
2023-04-18 19:14:44 +0200

    Force clang compiler in build_cmake_debug.sh script

--- ProjectFolder/scripts/build_cmake_debug.sh
+++ ProjectFolder/scripts/build_cmake_debug.sh
@@ -5,5 +5,5 @@ echo "Started building Debug CMAKE files..."
 rm -r build/debug &> /dev/null
 # This command also generates clang compile commands here:
 # ProjectFolder/build/debug/compile_commands.json
-cmake -SProjectFolder -Bbuild/debug -DCMAKE_BUILD_TYPE=Debug
+cmake -SProjectFolder -Bbuild/debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang
 echo "Finished building Debug CMAKE files!"
