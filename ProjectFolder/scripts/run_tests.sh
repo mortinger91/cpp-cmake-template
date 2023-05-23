b907a18007a7da4ddb41e4792c4953b0b6d3b0ea
Michele Emiliani <micheleemiliani91@gmail.com>
2023-05-23 10:21:00 +0200

    Make tests run in parallel adding -j option to CTest

--- ProjectFolder/scripts/run_tests.sh
+++ ProjectFolder/scripts/run_tests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-echo "Running tests..."
+if [ "$(uname -s)" == "Linux" ]; then
+	cpu_count=$(grep -c processor /proc/cpuinfo)
+elif [ "$(uname -s)" == "Darwin" ]; then
+	cpu_count=$(sysctl -a | grep "machdep.cpu.core_count: " | sed 's/^machdep.cpu.core_count: //')
+else
+	exit 1
+fi
+
+echo "Running tests using ${cpu_count} threads..."
 ProjectFolder/scripts/build_debug.sh && \
-ctest --output-on-failure --test-dir build/debug/tests
+ctest --output-on-failure --test-dir build/debug/tests -j$cpu_count
 RESULT=$?
 echo "Finished running tests..."
 exit "${RESULT}"
