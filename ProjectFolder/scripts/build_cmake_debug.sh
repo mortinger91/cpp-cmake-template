9edc03992245762a369095fd6d893529904bc2ca
Michele Emiliani <micheleemiliani91@gmail.com>
2023-08-22 10:37:44 +0200

    Remove get-cmake and run-cmake Github actions

--- ProjectFolder/scripts/build_cmake_debug.sh
+++ ProjectFolder/scripts/build_cmake_debug.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "Started building Debug CMAKE files..."
+
 # Clearing build folder if it exists
 rm -r build/debug &> /dev/null
+
 # This command also generates clang compile commands here:
 # ProjectFolder/build/debug/compile_commands.json
 cmake -SProjectFolder --preset Debug -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang
+
 echo "Finished building Debug CMAKE files!"
--- ProjectFolder/scripts/build_debug.sh
+++ ProjectFolder/scripts/build_debug.sh
@@ -10,4 +10,4 @@ fi
 
 cpu_count=$((cpu_count - 1))
 echo "Bulding Debug version using ${cpu_count} threads..."
-cmake --build build/debug/ -j$cpu_count
+cmake --build build/Debug/ -j$cpu_count
