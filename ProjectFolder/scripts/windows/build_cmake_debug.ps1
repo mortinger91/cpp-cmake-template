2bdb212a1770a29b548bf663340fb367e936b3be
mortinger91 <micheleemiliani91@gmail.com>
2023-10-17 02:25:10 +0200

    Add powershell scripts

---
+++ ProjectFolder/scripts/windows/build_cmake_debug.ps1
@@ -0,0 +1,10 @@
+# Script used to build CMake Debug files
+
+Write-Host "Started building Debug CMake files..."
+
+# Clearing build folder if it exists
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "build/debug"
+
+cmake -S ProjectFolder --preset Debug -G "Visual Studio 16 2019" -A x64
+
+Write-Host "Finished building Debug CMake files!"
---
+++ ProjectFolder/scripts/windows/create_release.ps1
@@ -0,0 +1,30 @@
+# Script used to build CMake Release files.
+# Calling this script without args will not build tests.
+# To build tests, call this script with "withTests" argument
+
+Write-Host "Started building Release CMake files..."
+
+# Clearing build folder if it exists
+Remove-Item -Path "build/release" -Recurse -ErrorAction SilentlyContinue
+
+Write-Host "Deciding if tests need to be built..."
+if ($args.Length -gt 0) {
+    if ($args[0] -eq "withTests") {
+        $BUILD_TESTS = "ON"
+        Write-Host "Building with tests"
+    }
+} else {
+    $BUILD_TESTS = "OFF"
+    Write-Host "Building without tests"
+}
+
+cmake -S ProjectFolder --preset Release -DBUILD_TESTS=$BUILD_TESTS
+
+Write-Host "Finished building Release CMake files!"
+
+$cpu_count = (Get-WmiObject -Class Win32_ComputerSystem).NumberOfLogicalProcessors
+$cpu_count = $cpu_count - 1
+
+Write-Host "Building Release version using $cpu_count threads..."
+cmake --build "build/Release" -j $cpu_count
+Write-Host "Finished building Release!"
