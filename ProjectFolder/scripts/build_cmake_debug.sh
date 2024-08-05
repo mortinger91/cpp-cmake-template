700945b799874c6c90742f8031fddd54ad0e0c57
mortinger91 <micheleemiliani91@gmail.com>
2024-08-05 11:46:06 +0200

    make pre commit hook executable

--- ProjectFolder/scripts/build_cmake_debug.sh
+++ ProjectFolder/scripts/build_cmake_debug.sh
@@ -9,6 +9,7 @@ rm -r build/debug &> /dev/null
 echo "Installing pre-commit hook"
 rm ./.git/hooks/pre-commit
 cp ./ProjectFolder/scripts/git_hooks/pre-commit ./.git/hooks/pre-commit
+chmod +x ./.git/hooks/pre-commit
 
 cmake -SProjectFolder --preset Debug -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang
 RESULT=$?
